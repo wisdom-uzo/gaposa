fc756d4cd6f6a0217e090863eb50e895c06f54f4
unknown <wisdomuzodimna@gmail.com>
2023-03-03 16:14:21 +0100

    first commit

--- pages/admin.js
+++ pages/admin.js
@@ -1,4 +1,5 @@
 
+import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import AdminLayout from "../components/AdminLayout";
@@ -26,7 +27,28 @@ function AbstractTable() {
 
   return (
     <AdminLayout>
-      ADMIN DASHBORD
+      <div className="p-2 bg-white mb-4">
+        <h1>ADMIN DASHBOARD</h1>
+      </div>
+
+      <div className="mt-10 flex flex-col items-center h-screen ">
+      <ul className=" flex gap-5 justify-center">
+
+        <li>
+          <Link href="/userTable" className=" rounded shadow bg-white font-bold p-5">
+            Registeired Users
+          </Link>
+        </li>
+        <li>
+          <Link href="/document" className=" rounded shadow bg-white font-bold p-5">
+            Documents
+          </Link>
+        </li>
+        <li>
+          <Link href="abstractTable" className=" rounded shadow bg-white font-bold p-5"> Abstract</Link>
+        </li>
+      </ul>
+      </div>
     </AdminLayout>
   );
 }
